21a93a6732ac88c090267ba405423311e8063fda
Allef sousa <allefsousa_1@hotmail.com>
2018-06-10 20:56:59 -0300

    criando a classe conexao com o banco de dados Mysql

---
+++ src/config/db.php
@@ -0,0 +1,17 @@
+<?php
+    class db{
+        // propriedades
+        private $dbhost = 'localhost';
+        private $dbuser = 'root';
+        private $dbpass = '';
+        private $dbname = 'api';
+
+        // conecxão
+        public function connect(){
+            $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;";
+            $dbConnection = new PDO(mysql_connect_str,$this->dbuser,$this->dbpass);
+            $dbConnection -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $dbConnection;
+        }
+
+    }
