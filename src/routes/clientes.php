74a1299c08e185d4f284d09a1ee8d4d4138f53e7
Allef sousa <allefsousa_1@hotmail.com>
2018-06-10 22:53:35 -0300

    Get Cliente OK

--- src/routes/clientes.php
+++ src/routes/clientes.php
@@ -3,8 +3,22 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 $app = new \Slim\App;
 
-$app->get('/api/customers', function(Request $request, Response $response){
-   
-        echo 'Get';
+
+// buscando todos os clientes
+$app->get('/api/clientes', function(Request $request, Response $response){
+    $sql = 'SELECT * FROM cli';
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $clie = $stmt->fetchAll(PDO::FETCH_OBJ); // retornando um array
+        $db = null;
+        echo json_encode($clie);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+    
     
 });
