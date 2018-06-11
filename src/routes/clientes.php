dae1b6810ffc35d4216cc52f9adef842a3d6598b
Allef sousa <allefsousa_1@hotmail.com>
2018-06-11 01:11:28 -0300

    Put ok

--- src/routes/clientes.php
+++ src/routes/clientes.php
@@ -3,6 +3,17 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 $app = new \Slim\App;
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 
 // Delete Customer
 $app->delete('/api/clientes/delete/{id}', function(Request $request, Response $response){
@@ -91,6 +102,43 @@ $app->post('/api/clientes/add', function(Request $request, Response $response){
         $stmt->bindParam(':estado',      $cliestado);
         $stmt->execute();
         echo '{"Msg": {"text": "Cliente Adicionado"}';
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+// Add Cliente
+$app->put('/api/clientes/update/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+    $clinome = $request->getParam('nome');
+    $clisobrenome = $request->getParam('sobrenome');
+    $clitelefone = $request->getParam('telefone');
+    $cliemail = $request->getParam('email');
+    $cliendereco = $request->getParam('endereco');
+    $clicidade = $request->getParam('cidade');
+    $cliestado = $request->getParam('estado');
+    $sql = "UPDATE cli SET  nome = :nome,
+                            sobrenome = :sobrenome,
+                            telefone = :telefone,
+                            email = :email,
+                            endereco = :endereco,
+                            cidade = :cidade,
+                            estado = :estado 
+                            where id='$id';";
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':nome', $clinome);
+        $stmt->bindParam(':sobrenome',  $clisobrenome);
+        $stmt->bindParam(':telefone',      $clitelefone);
+        $stmt->bindParam(':email',      $cliemail);
+        $stmt->bindParam(':endereco',    $cliendereco);
+        $stmt->bindParam(':cidade',       $clicidade);
+        $stmt->bindParam(':estado',      $cliestado);
+        $stmt->execute();
+        echo '{"Msg": {"text": "Cliente Atualizaado com sucesso"}';
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
