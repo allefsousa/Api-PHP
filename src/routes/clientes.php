7730d7d5d47d92767b3e9c5afa027d4a407c5cd7
Allef sousa <allefsousa_1@hotmail.com>
2018-06-10 23:50:23 -0300

    Post Cliente OK

--- src/routes/clientes.php
+++ src/routes/clientes.php
@@ -22,6 +22,8 @@ $app->get('/api/clientes', function(Request $request, Response $response){
     
     
 });
+
+
 $app->get('/api/clientes/{id}', function(Request $request, Response $response){
     $id = $request->getAttribute('id');
     $sql = "SELECT * FROM cli WHERE id = '$id';";
@@ -34,6 +36,37 @@ $app->get('/api/clientes/{id}', function(Request $request, Response $response){
         $customer = $stmt->fetch(PDO::FETCH_OBJ);
         $db = null;
         echo json_encode($customer);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Add Customer
+$app->post('/api/clientes/add', function(Request $request, Response $response){
+    $clinome = $request->getParam('nome');
+    $clisobrenome = $request->getParam('sobrenome');
+    $clitelefone = $request->getParam('telefone');
+    $cliemail = $request->getParam('email');
+    $cliendereco = $request->getParam('endereco');
+    $clicidade = $request->getParam('cidade');
+    $cliestado = $request->getParam('estado');
+    $sql = "INSERT INTO cli (nome,sobrenome,telefone,email,endereco,cidade,estado) VALUES
+    (:nome,:sobrenome,:telefone,:email,:endereco,:cidade,:estado)";
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':nome', $clinome);
+        $stmt->bindParam(':sobrenome',  $clisobrenome);
+        $stmt->bindParam(':telefone',      $clitelefone);
+        $stmt->bindParam(':email',      $cliemail);
+        $stmt->bindParam(':endereco',    $cliendereco);
+        $stmt->bindParam(':cidade',       $clicidade);
+        $stmt->bindParam(':estado',      $cliestado);
+        $stmt->execute();
+        echo '{"notice": {"text": "Customer Added"}';
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
